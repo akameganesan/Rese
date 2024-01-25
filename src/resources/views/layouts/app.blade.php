c0f8f0f28d712124a25668aa938862b9171f384b
akamegane <hayatoship@yahoo.co.jp>
2024-01-25 17:34:35 +0900

    menuボタンの中の線に苦戦中

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Attendance Management</title>
+    <title>Rese</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     @yield('css')
@@ -15,7 +15,8 @@
     <header class="header">
         <div class="header__inner">
             <div class="header-utilities">
-                <a href="/menu" class="contact__btn">-<span class="example2">-</span></a>
+                <a href="/menu" class="contact__btn">--<span class="example2">---</span><span
+                        class="example3">-</span></a>
                 <a class="header__logo" href="/">
                     Rese
                 </a>
