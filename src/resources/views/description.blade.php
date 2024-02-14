4c137d18a2bade0536cfb5416a67defed1616418
akamegane <hayatoship@yahoo.co.jp>
2024-02-14 13:22:45 +0900

    詳細詳細ページへ予約項目の設置2

--- src/resources/views/description.blade.php
+++ src/resources/views/description.blade.php
@@ -17,7 +17,10 @@
             <div class="header-utilities">
                 <a href="/" class="contact__btn2">X</a>
                 <a class="header__logo" href="/">
+
+
                     {{$shops->name}}
+
                 </a>
 
                 @if (Auth::check())
@@ -54,18 +57,8 @@
             @endif
         </ul>
 
-        <!--<div class="flex">
-    <div class="box">①</div>
-    <div class="box">②</div>
-    <div class="box">③</div>
-</div>-->
 
         <p class="p_font-size1">{{$shops->description}}</p>
-
-
-
-
-        <h1>{{$shops->name}}</h1>
 
     </div>
 
@@ -73,60 +66,78 @@
     <div class="container2">
         <div class="box">
             <div class="title__res">予約</div>
-            <form method='post' action='day'>
+            <form method='post' action='res'>
                 @csrf
-                <div class="day__size">
+                <div class="day__size2">
                     <input type="date" name="day">
                 </div>
-                <button type="submit">送信</button>
-            </form>
-            <form method='post' action='time'>
-                @csrf
-                <div class="day__size">
+
+                <div class="day__size1">
                     <input type="time" name="time">
                 </div>
-                <button type="submit">送信</button>
-            </form>
-            <div class="day__size">
-                <form method='post' action='num'>
-                    @csrf
+
+                <div class="day__size1">
                     <select name='num'>
                         <option value='1'>１人</option>
                         <option value='2'>２人</option>
                         <option value='3'>３人</option>
+                        <option value='4'>４人</option>
+                        <option value='5'>５人</option>
+                        <option value='6'>６人</option>
+                        <option value='7'>７人</option>
+                        <option value='8'>８人</option>
+                        <option value='9'>９人</option>
+                        <option value='10'>１０人</option>
                     </select>
-                    <button type="submit">送信</button>
-                </form>
-            </div>
-            <div class="box2">
-                <div class="confirm">
-                    @if(empty($day))
-                    @else
-                    <h2>Date {{$day}}</h2>
-                    @endif
-                    @if(empty($time))
-                    @else
-                    <h2>Time {{$time}}</h2>
-                    @endif
-                    @if(empty($num))
-                    @else
-                    <h2>Number {{$num}}</h2>
-                    @endif
                 </div>
+                <div class="day__size3">
+                    <input type="hidden" type="text" name="shoID" value="{{$shops->id}}">
+                </div>
+                <div class="button_xy">
+                    <button type="submit">送信</button>
+                </div>
+
+            </form>
+        </div>
+
+
+        <div class="box2">
+            <div class="confirm">
+                @if(empty($shops->name))
+                @else
+                <h2>Shop {{$shops->name}}</h2>
+                @endif
+                @if(empty($day))
+                <h2>Date </h2>
+                @else
+                <h2>Date {{$day}}</h2>
+                @endif
+                @if(empty($time))
+                <h2>Time </h2>
+                @else
+                <h2>Time {{$time}}</h2>
+                @endif
+                @if(empty($num))
+                <h2>Number </h2>
+                @else
+                <h2>Number {{$num}}</h2>
+                @endif
+
             </div>
-
-
         </div>
         <div class="box1">
 
         </div>
 
-
-
-
-
     </div>
 
+
+
+
+
+
 </div>
+
+
 
 @endsection
