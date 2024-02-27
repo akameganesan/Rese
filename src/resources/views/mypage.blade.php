3f8b50d8c4737d7428ffd5a5b323364a854e552a
akamegane <hayatoship@yahoo.co.jp>
2024-02-27 22:52:03 +0900

    見た目改善中mypage

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -61,48 +61,49 @@
 
 
 
+<div class="new_container1">
+
+    <div class="container__color">
+        <div class="flex__item4">
+            <div class="background">
+                <div class="container">
+                    <div class="flex__item4">
+                        <div class="container1">
+                            <div class="title__res2">予約状況</div>
 
 
 
-<div class="flex__item4">
-    <div class="background">
-        <div class="container">
-            <div class="flex__item4">
-                <div class="container1">
-                    <div class="title__res2">予約状況</div>
+                            @foreach($reserv as $re)
+                            @php
+                            //$Date = $re->start_at;
+                            //$day = $Date->format('Y-m-d');
+                            //$time = $Date->format('H:i');
+                            @endphp
 
+                            <!--<div class="box">-->
 
-
-                    @foreach($reserv as $re)
-                    @php
-                    //$Date = $re->start_at;
-                    //$day = $Date->format('Y-m-d');
-                    //$time = $Date->format('H:i');
-                    @endphp
-
-                    <!--<div class="box">-->
-
-                    <div class="confirm">
-                        <div class="box__move">
-                            <div class="box">
-                                <div class="icon__flex">
-                                    <div class="icon__left">
-                                        <img class="size__icon" src="{{ asset('img/時計の無料アイコン (1).svg') }}" alt="">
-                                    </div>
-                                </div>
-
-                                <div class="title__res3">予約1
-                                </div>
-
-                                <form action="/delete" type="text" name="shoID" method="post">
-                                    @csrf
-                                    <div class="flex__item2">
-                                        <input type="hidden" name="shoID1" value="{{$re->shop_id}}" size="10">
-                                        <div class="form__button">
-                                            <button class="form__button-submit" type="submit">X</button>
+                            <div class="confirm">
+                                <div class="box__move">
+                                    <div class="box">
+                                        <div class="icon__flex">
+                                            <div class="icon__left">
+                                                <img class="size__icon" src="{{ asset('img/時計の無料アイコン (1).svg') }}"
+                                                    alt="">
+                                            </div>
                                         </div>
-                                    </div>
-                                </form>
+
+                                        <div class="title__res3">予約1
+                                        </div>
+
+                                        <form action="/delete" type="text" name="shoID" method="post">
+                                            @csrf
+                                            <div class="flex__item2">
+                                                <input type="hidden" name="shoID1" value="{{$re->shop_id}}" size="10">
+                                                <div class="form__button">
+                                                    <button class="form__button-submit" type="submit">X</button>
+                                                </div>
+                                            </div>
+                                        </form>
 
 
 
@@ -111,41 +112,41 @@
 
 
 
-                                @if(empty($re->name))
-                                @else
-                                <h2>Shop　　　　{{$re->name}}</h2>
-                                @endif
+                                        @if(empty($re->name))
+                                        @else
+                                        <h2>Shop　　　　{{$re->name}}</h2>
+                                        @endif
 
-                                @if(empty($re->start_at))
-                                <h2>Date </h2>
-                                @else
-                                @php
-                                $date = \Carbon\Carbon::create($re->start_at);
-                                $formattedDate = $date->format('Y-m-d');
-                                $formattedTime = $date->format('H:i');
-                                @endphp
+                                        @if(empty($re->start_at))
+                                        <h2>Date </h2>
+                                        @else
+                                        @php
+                                        $date = \Carbon\Carbon::create($re->start_at);
+                                        $formattedDate = $date->format('Y-m-d');
+                                        $formattedTime = $date->format('H:i');
+                                        @endphp
 
-                                <h2>Date　　　　{{$formattedDate}}</h2>
-                                @endif
-                                @if(empty($re->start_at))
-                                <h2>Time </h2>
-                                @else
-                                <h2>Time　　　　{{$formattedTime}}</h2>
-                                @endif
-                                @if(empty($re->member_num))
-                                <h2>Number </h2>
-                                @else
-                                <h2>Number　　　　{{$re->member_num}}</h2>
-                                @endif
+                                        <h2>Date　　　　{{$formattedDate}}</h2>
+                                        @endif
+                                        @if(empty($re->start_at))
+                                        <h2>Time </h2>
+                                        @else
+                                        <h2>Time　　　　{{$formattedTime}}</h2>
+                                        @endif
+                                        @if(empty($re->member_num))
+                                        <h2>Number </h2>
+                                        @else
+                                        <h2>Number　　　　{{$re->member_num}}</h2>
+                                        @endif
 
-                                <!--<div class="icon__flex">
+                                        <!--<div class="icon__flex">
                                 <div class="icon__left">
                                     <img class="icon_1" src="{{ asset('img/時計無料アイコン.svg') }}" alt="">
                                 </div>
                             </div>-->
 
 
-                                <!--<form action="/delete" type="text" name="shoID" method="post">
+                                        <!--<form action="/delete" type="text" name="shoID" method="post">
                                 @csrf
                                 <div class="flex__item2">
                                     <input type="hidden" name="shoID1" value="{{$re->shop_id}}" size="10">
@@ -155,102 +156,110 @@
                                 </div>
                             </form>-->
 
-                            </div>
-                        </div>
-                    </div>
-                    <!--</div>-->
-                    @endforeach
-
-
-
-                </div>
-
-
-                <div class="container2">
-                    <div class="card__margin">
-                        <div class="flex__item blog-wrap">
-
-                            @foreach($fav as $fa)
-                            @php
-                            $s = $fa->id;
-                            $p = $fa->shops_id;
-                            @endphp
-                            @if($fa->id == $fa->shops_id)
-                            <a class="blog-wrap__item" href="">
-                                <div class="register__shadow2">
-                                    <img src="{{$fa->img_url}}" alt="" class="blog-wrap__item-eyecatch">
-                                </div>
-                                <div class="register__shadow">
-                                    <div class="blog-wrap__item-content">
-                                        <h3 class="blog-wrap__item-content-ttl">{{$fa->name}}</td>
-                                        </h3>
-                                        <ul class="flex__item2">
-                                            @if($fa->area_id == 1)
-                                            <li class="blog-wrap__item-content-tag">#東京</li>
-                                            @elseif($fa->area_id == 2)
-                                            <li class="blog-wrap__item-content-tag">#大阪府</li>
-                                            @elseif($fa->area_id == 3)
-                                            <li class="blog-wrap__item-content-tag">#福岡県</li>
-                                            @endif
-
-                                            @if($fa->genre_id == 1)
-                                            <li class="blog-wrap__item-content-tag2">#寿司</li>
-                                            @elseif($fa->genre_id == 2)
-                                            <li class="blog-wrap__item-content-tag2">#焼肉</li>
-                                            @elseif($fa->genre_id == 3)
-                                            <li class="blog-wrap__item-content-tag2">#ラーメン</li>
-                                            @elseif($fa->genre_id == 4)
-                                            <li class="blog-wrap__item-content-tag2">#イタリアン</li>
-                                            @elseif($fa->genre_id == 5)
-                                            <li class="blog-wrap__item-content-tag2">#居酒屋</li>
-                                            @endif
-                                        </ul>
-
-                                        <form action="/detail" type="text" name="shoID" method="post">
-                                            @csrf
-                                            <div class="flex__item2">
-                                                <input type="hidden" name="shoID1" value="{{$fa->id}}" size="10">
-                                                <div class="form__button1">
-                                                    <button class="form__button-submit" type="submit">詳しく見る</button>
-                                                </div>
-                                            </div>
-                                        </form>
-
-
-
-                                        @if (Auth::check())
-
-                                        <form action="/create3" type="text" name="shoID" method="post">
-                                            @csrf
-                                            <input type="hidden" name="shID1" value="{{$fa->id}}" size="10">
-                                            <div class="heart__margin">
-                                                <div class="button_none">
-                                                    <button class="heart1" type="submit"></button>
-                                                </div>
-                                            </div>
-                                        </form>
-
-                                        @foreach($shops as $shop)
-
-                                        @if($s == $p)
-                                        <form action="/remove" type="text" name="shoID" method="post">
-                                            @csrf
-                                            <input type="hidden" name="shID1" value="{{$shop->id}}" size="10">
-                                            <div class="heart__margin">
-                                                <div class="button_none">
-                                                    <button class="heart" type="submit"></button>
-                                                </div>
-                                            </div>
-                                        </form>
-                                        @else
-                                        @endif
-                                        @endforeach
-                                        @endif
                                     </div>
                                 </div>
-                            </a>
-                            @endif
+                            </div>
+                            <!--</div>-->
                             @endforeach
+
+
+
+                        </div>
+
+
+                        <div class="container2">
+                            <div class="card__margin">
+                                <div class="flex__item blog-wrap">
+                                    <div class="saizensen">
+                                        @foreach($fav as $fa)
+                                        @php
+                                        $s = $fa->id;
+                                        $p = $fa->shops_id;
+                                        @endphp
+                                        @if($fa->id == $fa->shops_id)
+                                        <a class="blog-wrap__item" href="">
+                                            <div class="register__shadow2">
+                                                <img src="{{$fa->img_url}}" alt="" class="blog-wrap__item-eyecatch">
+                                            </div>
+                                            <div class="register__shadow">
+                                                <div class="blog-wrap__item-content">
+                                                    <h3 class="blog-wrap__item-content-ttl">{{$fa->name}}</td>
+                                                    </h3>
+                                                    <ul class="flex__item2">
+                                                        @if($fa->area_id == 1)
+                                                        <li class="blog-wrap__item-content-tag">#東京</li>
+                                                        @elseif($fa->area_id == 2)
+                                                        <li class="blog-wrap__item-content-tag">#大阪府</li>
+                                                        @elseif($fa->area_id == 3)
+                                                        <li class="blog-wrap__item-content-tag">#福岡県</li>
+                                                        @endif
+
+                                                        @if($fa->genre_id == 1)
+                                                        <li class="blog-wrap__item-content-tag2">#寿司</li>
+                                                        @elseif($fa->genre_id == 2)
+                                                        <li class="blog-wrap__item-content-tag2">#焼肉</li>
+                                                        @elseif($fa->genre_id == 3)
+                                                        <li class="blog-wrap__item-content-tag2">#ラーメン</li>
+                                                        @elseif($fa->genre_id == 4)
+                                                        <li class="blog-wrap__item-content-tag2">#イタリアン</li>
+                                                        @elseif($fa->genre_id == 5)
+                                                        <li class="blog-wrap__item-content-tag2">#居酒屋</li>
+                                                        @endif
+                                                    </ul>
+
+                                                    <form action="/detail" type="text" name="shoID" method="post">
+                                                        @csrf
+                                                        <div class="flex__item2">
+                                                            <input type="hidden" name="shoID1" value="{{$fa->id}}"
+                                                                size="10">
+                                                            <div class="form__button1">
+                                                                <button class="form__button-submit"
+                                                                    type="submit">詳しく見る</button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+
+
+
+                                                    @if (Auth::check())
+
+                                                    <form action="/create3" type="text" name="shoID" method="post">
+                                                        @csrf
+                                                        <input type="hidden" name="shID1" value="{{$fa->id}}" size="10">
+                                                        <div class="heart__margin">
+                                                            <div class="button_none">
+                                                                <button class="heart1" type="submit"></button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+
+                                                    @foreach($shops as $shop)
+
+                                                    @if($s == $p)
+                                                    <form action="/remove" type="text" name="shoID" method="post">
+                                                        @csrf
+                                                        <input type="hidden" name="shID1" value="{{$shop->id}}"
+                                                            size="10">
+                                                        <div class="heart__margin">
+                                                            <div class="button_none">
+                                                                <button class="heart" type="submit"></button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+                                                    @else
+                                                    @endif
+                                                    @endforeach
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </a>
+                                        @endif
+                                        @endforeach
+                                        <div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -258,6 +267,4 @@
         </div>
     </div>
 </div>
-
-
 @endsection
